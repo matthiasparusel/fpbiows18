e105e5f86680c08a5d9645e43de234cfab19e5dd
Charlotte Diederich <charlotte.diederich@tu-dortmund.de>
2019-01-17 16:23:48 +0100

    Stripplot

--- scripts/stripplot_top20.py
+++ scripts/stripplot_top20.py
@@ -9,6 +9,21 @@ sleuth_table = sleuth_table.head(20)
 kallisto_table = pd.read_table("temp/counts_normalized.tsv", delim_whitespace=True)
 
 p_value_table = sleuth_table[['target_id', 'pval']]
-counts_table = kallisto_table[['target_id', 'est']]
+counts_table = kallisto_table[['target_id', 'est_counts', 'sample']]
 
-print(p_value_table)
+
+
+merged_table = counts_table.merge(p_value_table, left_on='target_id', right_on='target_id', how='inner')
+
+
+plt.figure(figsize=[12,8])
+ax = sns.stripplot(x='est_counts', y='pval', hue='target_id', data=merged_table)
+
+plt.xticks(rotation=45)
+plt.grid()
+plt.tight_layout()
+plt.savefig('results/stripplot.pdf')
+
+#print(p_value_table)
+
+#print(merged_table)
