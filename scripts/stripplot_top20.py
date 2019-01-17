d53b7ac834179a0c91adc596374c8bd23a31f15b
Charlotte Diederich <charlotte.diederich@tu-dortmund.de>
2019-01-17 15:18:41 +0100

    env geändert

--- scripts/stripplot_top20.py
+++ scripts/stripplot_top20.py
@@ -3,11 +3,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 sleuth_table = pd.read_table("temp/sleuth_table.tsv", delim_whitespace=True)
-
 sleuth_table = sleuth_table.sort_values('pval', ascending=True)
 sleuth_table = sleuth_table.head(20)
 
-
+kallisto_table = pd.read_table("temp/counts_normalized.tsv", delim_whitespace=True)
 
 p_value_table = sleuth_table[['target_id', 'pval']]
+counts_table = kallisto_table[['target_id', 'est']]
+
 print(p_value_table)
