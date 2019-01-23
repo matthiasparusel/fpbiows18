343caf612671242a0bd198bb0769c7563fca0b5c
Matthias Parusel <matthias.parusel@tu-dortmund.de>
2019-01-23 22:09:46 +0100

    Fixed boxenplot graphic and added condition to it

--- scripts/boxenplot.py
+++ scripts/boxenplot.py
@@ -20,13 +20,28 @@ kal_results = pd.concat(kal_results)
 sle_results = pd.read_table(snakemake.input[1], delim_whitespace=True)
 
 # Finally plot the thing
+sns.set(style='darkgrid')
 fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(9, 4), sharey=True)
 sns.boxenplot(x='sample', y='est_counts', data=kal_results, ax=axes[0])
 axes[0].set_title('counts of all samples')
 sns.boxenplot(x='sample', y='est_counts', data=sle_results, ax=axes[1])
 axes[1].set_title('normalized counts of all samples')
+
+# write sample together with condtion as x labels
+labels = [item.get_text() for item in axes[0].get_xticklabels()]
+conditions = list(samples['condition'])
+labels = list(map(', '.join, zip(labels, conditions)))
+
 for ax in axes:
+    ax.set_xticklabels(labels)
     ax.yaxis.grid(True)
-    
-plt.xticks(rotation=45)
+for ax in axes:
+    plt.sca(ax)
+    plt.xticks(rotation=70)
+
+
+labels = [item.get_text() for item in axes[0].get_xticklabels()]
+conditions = list(samples['condition'])
+labels = list(map(', '.join, zip(labels, conditions)))
+
 plt.savefig(snakemake.output[0],bbox_inches="tight")
