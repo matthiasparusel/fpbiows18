bb9a1c5174d27ffe39cf6ddede393ef57b946262
Charlotte Diederich <charlotte.diederich@tu-dortmund.de>
2019-01-24 16:03:25 +0100

    stripplot achsen

--- scripts/stripplot_top20.py
+++ scripts/stripplot_top20.py
@@ -19,6 +19,14 @@ merged_table = counts_table.merge(p_value_table, left_on='target_id', right_on='
 plt.figure(figsize=[12,8])
 ax = sns.stripplot(x='est_counts', y='pval', hue='target_id', data=merged_table)
 
+y_lim = p_value_table.at[19,'pval']
+plt.ylim(0, y_lim*1.2)
+ax.xaxis.set_major_locator(plt.AutoLocator())
+ax.yaxis.set_major_locator(plt.AutoLocator())
+
+#plt.ticklabel_format(style='plain', axis='x')
+
+ax.legend(loc='best')
 plt.xticks(rotation=45)
 plt.grid()
 plt.tight_layout()
